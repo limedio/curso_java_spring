0a7968d83ca51cd5920d5ca70e401862edec3ee5
luis <luisimedio@gmail.com>
2019-05-08 12:52:51 +0200

    practicas

--- pkg02_colecciones/src/pkg02_colecciones/ProbandoFechas.java
+++ pkg02_colecciones/src/pkg02_colecciones/ProbandoFechas.java
@@ -6,16 +6,31 @@ import java.util.GregorianCalendar;
 
 
 public class ProbandoFechas {
-    
- 
     //Metodo estático. Función externa que no tiene que ver con la clase, son funciones generales que hacen cosas generales que no tienen 
     //nada que ver con la aplicación.
     
     public static void probar(){
-        GregorianCalendar fecha = new GregorianCalendar();
-        fecha.set(2019, 5 ,8);
+        FechaSinMinutos fecha = new FechaSinMinutos();
+        fecha.set(2019, 6, 2);
         System.out.println("Fecha: " + fecha.toString());
         Date fechaDate = fecha.getTime();
         System.out.println("Fecha: " + fechaDate.toString());
-    }
+        
+        FechaSinMinutos miCumple;
+        miCumple = new FechaSinMinutos(2019, 6, 2);
+            
+        System.out.println("fecha" + fecha);
+        System.out.println("micumple" + miCumple);
+        
+        
+        if (miCumple.getTimeWithoutSeconds().after(fecha.getTimeWithoutSeconds())){
+            System.out.println("Aun no cumples");
+        } else if (miCumple.getTimeWithoutSeconds().before(fecha.getTimeWithoutSeconds())){
+            System.out.println("Ya cumpliste");
+        }else if (miCumple.equals(fecha)){
+            System.out.println("¡Cumpleaños feliz!");
+        }
+            
+        
+        }
 }
