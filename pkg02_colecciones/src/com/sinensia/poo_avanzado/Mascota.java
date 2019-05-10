82cec9224e191ed5b94337b39ae62cfb26a5135b
luis <luisimedio@gmail.com>
2019-05-10 17:03:10 +0200

    ddfg

--- pkg02_colecciones/src/com/sinensia/poo_avanzado/Mascota.java
+++ pkg02_colecciones/src/com/sinensia/poo_avanzado/Mascota.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.sinensia.poo_avanzando;
+package com.sinensia.poo_avanzado;
 
 import com.sinensia.Cliente;
 
